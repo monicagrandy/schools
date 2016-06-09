2a5d808d523433ae2d270c1beec9a1d4f1f56ab9
Monica Grandy <monica.grandy@gmail.com>
2016-06-08 17:14:46 -0700

    [feature] adds school store and updates main.jsx

--- app/main.jsx
+++ app/main.jsx
@@ -1,11 +1,15 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var SchoolsList = require("./components/SchoolsList.jsx");
+var schoolsStore = require("./stores/schoolsStore");
+var _schools = schoolsStore.getSchools();
+schoolsStore.onChange(function(schools){
+    _schools = schools;
+    render();
+});
 
-var _schools = [{name:"Lovedale",tagline:"this is a wonderful school"},
-                {name:"Bishop",tagline:"Another great school"}];
-                
 function render(){
     ReactDOM.render(<SchoolsList schools={_schools} />, document.getElementById("container"));    
 }
+
 render();
